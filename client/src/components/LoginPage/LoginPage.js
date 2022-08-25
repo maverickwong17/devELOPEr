b8f36f075b7ca9f7de6e78d2ec97bf87b5333e31
smandla <35283559+smandla@users.noreply.github.com>
2022-08-24 23:11:03 -0700

    create sign in page

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -7,17 +7,17 @@ const LoginPage = () => {
       <Form className="login_form">
         {/* <span> */}
         <h2>Welcome Back</h2>
-        <h4>Sign In to continue</h4>
-        <input className="login_input" placeholder="username" />
-        <input className="login_input" placeholder="password" />
-        <span>
+        <h4 className="side_text">Sign In to continue</h4>
+        <input className="login_input" placeholder="Username" />
+        <input type="password" className="login_input" placeholder="Password" />
+        <span className="remember_me">
           {" "}
-          <input type="checkbox" /> Remember me
+          <input className="checkbox" type="checkbox" /> Remember me
         </span>
         <Button className="signin_btn">SIGN IN</Button>
         <p>Did you forget your password?</p>
-        <Button>Reset here</Button>
-        <Button>SIGN UP</Button>
+        <Button className="reset_btn">Reset here</Button>
+        <Button className="signup_btn">SIGN UP</Button>
         {/* </span> */}
       </Form>
     </Container>

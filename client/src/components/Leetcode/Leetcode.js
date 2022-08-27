4186dc3ab5c0a0dafcbf6c7d51bdf0a910738ae8
smandla <35283559+smandla@users.noreply.github.com>
2022-08-27 11:23:36 -0700

    using codemirror for text editor and judge0 for running code

--- client/src/components/Leetcode/Leetcode.js
+++ client/src/components/Leetcode/Leetcode.js
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { Col, Row, Badge, Button } from "react-bootstrap";
 import { FiCode } from "react-icons/fi";
 import { FaCog, FaExclamationCircle, FaUserAlt } from "react-icons/fa";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { cpp } from "@codemirror/lang-cpp";
+import { java } from "@codemirror/lang-java";
+import { python } from "@codemirror/lang-python";
 import "./Leetcode.css";
 const Leetcode = () => {
   const [input, setInput] = useState(localStorage.getItem("input") || "");
@@ -30,7 +35,7 @@ const Leetcode = () => {
       },
       body: JSON.stringify({
         language_id: 63,
-        source_code: encode('console.log("hello, world");'),
+        source_code: encode(user_input),
         stdin: encode(""),
       }),
     };
@@ -73,18 +78,141 @@ const Leetcode = () => {
       console.log(output);
     } else if (jsonGetSolution.stderr) {
       const error = atob(jsonGetSolution.stderr);
+      console.log(error);
     } else {
       const comp_error = atob(jsonGetSolution.compile_output);
+      console.log(comp_error);
     }
   };
+  const onChange = useCallback((value, viewUpdate) => {
+    console.log("value:", value);
+    setUserInput(value);
+  }, []);
   return (
     <>
-      <div>
-        <Button type="submit" className="btn-danger" onClick={runCodeHandler}>
-          <FaCog /> Run
-        </Button>
-      </div>
+      <Row
+        className="grid"
+        style={{
+          height: "80vh",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Col
+          md={5}
+          style={{
+            marginTop: "10px",
+          }}
+        >
+          <Row>
+            <Col
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <h3 style={{ color: "white" }}>Coding Challenge</h3>
+              <p style={{ color: "white" }}>
+                You are given an integer n. We reorder the digits in any order
+                (including the original order) such that the leading digit is
+                not zero. Return true if and only if we can do this so that the
+                resulting number is a power of two.
+              </p>
+              <hr style={{ fill: "white" }} />
+            </Col>
+          </Row>
+          <Row>
+            <Col
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <CodeMirror
+                required
+                name="solution"
+                id="source"
+                value={input || "function(x){\n}"}
+                // value="console.log('hello world!');"
+                height="450px"
+                theme="dark"
+                extensions={[
+                  javascript({ jsx: true }),
+                  cpp(),
+                  java(),
+                  python(),
+                ]}
+                onChange={onChange}
+              />
+
+              {/* <textarea
+                onChange={textInputHandler}
+                className="source"
+                style={{ height: "500px", padding: 0, margin: 0 }}
+              ></textarea> */}
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "right",
+                  justifyContent: "right",
+                }}
+              >
+                <Button
+                  type="submit"
+                  className="run_btn"
+                  style={{
+                    marginTop: "10px",
+                  }}
+                  onClick={runCodeHandler}
+                >
+                  <FaCog /> Run
+                </Button>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+        {/* <hr /> */}
+        <Col
+          md={5}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            background: "blue",
+          }}
+        >
+          <div>
+            <div
+              style={{
+                marginTop: "10px",
+                display: "flex",
+                justifyContent: "right",
+                alignItems: "right",
+              }}
+            >
+              <select
+                value={language_id}
+                onChange={(e) => setLanguageId(e.target.value)}
+                id="tags"
+                className="form-control form-inline mb-2 language"
+              >
+                <option value="54">C++</option>
+                <option value="62">Java</option>
+                <option value="71">Python</option>
+                <option value="63">JavaScript</option>
+              </select>
+            </div>
+          </div>{" "}
+          <div>
+            <Badge className="badge badge-info heading my-2 ">
+              <FaExclamationCircle /> Output
+            </Badge>
+          </div>
+          <textarea id="output"></textarea>
+        </Col>
+      </Row>
     </>
+
+    // </>
   );
 };
 

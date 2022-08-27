786f5f9ba0aa6e57a8af77be671fe89e0a11cd29
Matt F <fiaschettima@gmail.com>
2022-08-27 14:41:36 -0400

    landing container

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,10 +1,29 @@
 import React from "react";
-
+import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import './landing.css'
 const LandingPage = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
+    <Container>
+    <div className="landingContainer">
+      <Row className="align-items-center ">
+        <Col>
+        <div className="tag">
+          <h1>Where They Always Promise To Callback()</h1>
+          <Link
+            className=" btn matchBtn"
+            to = {'/signup'}
+          >
+            Find Your Match Today
+          </Link>
+        </div>
+        </Col>
+        <Col>
+          <img src="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg"></img>
+        </Col>
+      </Row>
     </div>
+    </Container>
   );
 };
 

1dde74599ca4ede8da964347fe95cfe6da2b0e0f
Matt F <fiaschettima@gmail.com>
2022-08-31 13:47:52 -0400

    add react to swiper

--- client/src/components/home/homepage.js
+++ client/src/components/home/homepage.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import  Swiper  from '../Swipe/swipe'
-
+import { Col, Container, Row } from "react-bootstrap";
 
 const Homepage = () => {
   return (
-    <div>
-        <Swiper/>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Swiper/>
+        </Col>
+      </Row>
+      <Row>
+          <Col>
+            <h3>
+              Find your pair programmer now
+            </h3>
+            <p>Swipe left for no right for yes</p>
+          </Col>
+      </Row>
+    </Container>
   )
 }
 

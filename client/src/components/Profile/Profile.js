beadea4fb7dae0920dd6ba75e2253f835501d324
smandla <35283559+smandla@users.noreply.github.com>
2022-09-01 11:34:45 -0700

    job and location styling in profilepage

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
+import React from "react";
+import { useQuery } from "@apollo/client";
 import { Navigate, useParams } from "react-router-dom";
 import { Col, Row, Container, Button } from "react-bootstrap";
-import { QUERY_USER, QUERY_ME } from '../../utils/queries';
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
-import InterestList from './InterestList';
-import { ReactComponent as Github } from '../assets/svg/github.svg'
-import { ReactComponent as LinkedIn } from '../assets/svg/linkedin.svg'
-import Auth from '../../utils/auth';
+import { QUERY_USER, QUERY_ME } from "../../utils/queries";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import InterestList from "./InterestList";
+import { ReactComponent as Github } from "../assets/svg/github.svg";
+import { ReactComponent as LinkedIn } from "../assets/svg/linkedin.svg";
+import Auth from "../../utils/auth";
 
 import "./Profile.css";
 
@@ -34,44 +34,73 @@ const Profile = () => {
       </h4>
     );
   }
-  console.log(user)
+  console.log(user);
   let interests = user.interest;
   let images = user.images;
   // console.log(interests)
 
   return (
-    <Container className='profileContainer'>
+    <div className="profileContainer">
       {/* <div className='profileContainer'> */}
-      <Row className='profileHeader'>
+      <Row className="profileHeader">
         <div>
-          <h2 className='h2'>{`${user.firstName}`} <span>{`${user.age}`}</span></h2>
+          <h2 className="h2">
+            {`${user.firstName}`} <span>{`${user.age}`}</span>
+          </h2>
           {/* <p className='age'>{`${user.age}`}</p> */}
         </div>
       </Row>
-      <Row className='profileHeader2'>
-        <p className='job'>{`${user.job}`}
+      <Row className="profileHeader2">
+        <span className="job">
+          {`${user.job}`}
           {/* <span class='city'>{`${user.location}`}</span> */}
-        </p>
-        <p className='city'>{`${user.location}`}</p>
+        </span>
+        <span className="city">{`${user.location}`}</span>
       </Row>
       <Row>
-        <Col l={{ order: 1 }} className='containerLeftColumn'>
-          <h3 className=''>About Me</h3>
-          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-          <h3 className=''>Interests</h3>
+        <Col l={{ order: 1 }} className="containerLeftColumn">
+          <h3 className="">About Me</h3>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum."
+          </p>
+          <h3 className="">Interests</h3>
           <div>
             <InterestList interests={interests} />
           </div>
           <div>
-            <a href={user.github} target='_blank' rel='noreferrer noopener' className='container profileLinkButtons'><Github />
+            <a
+              href={user.github}
+              target="_blank"
+              rel="noreferrer noopener"
+              className="container profileLinkButtons"
+            >
+              <Github />
             </a>
-            <a href={user.linkedin} target='_blank' rel='noreferrer noopener' className='container profileLinkButtons'><LinkedIn />
+            <a
+              href={user.linkedin}
+              target="_blank"
+              rel="noreferrer noopener"
+              className="container profileLinkButtons"
+            >
+              <LinkedIn />
             </a>
           </div>
         </Col>
-        <Col l={{ order: 2 }} className='containerRightColumn'>
+        <Col l={{ order: 2 }} className="containerRightColumn">
           <ResponsiveMasonry
-            columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
+            columnsCountBreakPoints={{
+              350: 1,
+              750: 2,
+              900: 3,
+              1200: 4,
+              // 1400: 5,
+            }}
           >
             <Masonry>
               {images.map((image, i) => (
@@ -87,8 +116,8 @@ const Profile = () => {
         </Col>
       </Row>
       {/* </div> */}
-    </Container>
-  )
-}
+    </div>
+  );
+};
 
 export default Profile;

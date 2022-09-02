9a54e49821c2742c5882b835e411e58ca3798d38
smandla <35283559+smandla@users.noreply.github.com>
2022-09-02 10:32:56 -0700

    css fix for interest button profile page

--- client/src/components/Profile/InterestList.js
+++ client/src/components/Profile/InterestList.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import InterestItem from './InterestItem';
-
+import React from "react";
+import InterestItem from "./InterestItem";
+import "./InterestList.css";
 const InterestList = ({ interests }) => {
-    console.log('interests:', interests);
+  console.log("interests:", interests);
 
-    const renderedList = interests.map((interest, i) => {
-        return <InterestItem key={i} interest={interest} />;
-    });
+  const renderedList = interests.map((interest, i) => {
+    return <InterestItem key={i} interest={interest} />;
+  });
 
-        return <div className="ui relaxed divided list">{renderedList}</div>;
+  return (
+    <div className="ui relaxed divided list interestList">{renderedList}</div>
+  );
 };
 
 export default InterestList;

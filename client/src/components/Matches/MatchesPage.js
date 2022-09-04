a10b08d7b7dfd236834640791b875aef25054e51
Phillip <phillip.besse@gmail.com>
2022-09-03 23:54:46 -0400

    fix swipe card and button location bug

--- client/src/components/Matches/MatchesPage.js
+++ client/src/components/Matches/MatchesPage.js
@@ -66,30 +66,6 @@ const MatchesPage = () => {
   }
   var matches = map(userArr)
 
-
-  const db = [
-    {
-      name: "Richard Hendricks",
-      url: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80",
-    },
-    {
-      name: "Erlich Bachman",
-      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFR2mVAx8bkghX04MOTSwnnfoUSD8BlCGNEA&usqp=CAU",
-    },
-    {
-      name: "Monica Hall",
-      url: "https://media.vanityfair.com/photos/61fc0e53f5c5a2437d283af6/master/w_2560%2Cc_limit/1199282262",
-    },
-    {
-      name: "Jared Dunn",
-      url: "https://pittnews.com/wp-content/uploads/2021/01/C_Cody-Ko_Via.jpg",
-    },
-    {
-      name: "Dinesh Chugtai",
-      url: "https://media1.popsugar-assets.com/files/thumbor/gLSZSLj5vDzOv6kno4SYK5I6fLc/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2022/06/03/892/n/46712211/627a8ceb4384d234_MSDTOGU_EC018/i/Tom-Cruise-as-Pete-Maverick-Mitchell-Then.jpg",
-    },
-  ];
-
   return (
     <Row className="grid_matches">
       <div className="matches">
--- client/src/components/Swipe/swipe.js
+++ client/src/components/Swipe/swipe.js
@@ -1,146 +1,35 @@
-import React, { useState, useMemo, useRef } from "react";
-import TinderCard from "react-tinder-card";
-import "./swipe.css";
-import { AiFillHeart } from "react-icons/ai";
-import { MdCancel } from "react-icons/md";
-import { FaUndo } from "react-icons/fa";
+import React, { useState, useMemo, useRef } from 'react';
+import TinderCard from 'react-tinder-card';
+// import "./swipeCard/swipe.css";
+import { AiFillHeart } from 'react-icons/ai';
+import { MdCancel } from 'react-icons/md';
+import { FaUndo } from 'react-icons/fa';
 import InterestButton from "../InterestButton/InterestButton";
 import data from "../../data/interestsJson";
-
-import { Row } from "react-bootstrap";
+import { Row } from 'react-bootstrap';
 import { QUERY_ALL_USER, QUERY_ME } from "../../utils/queries";
-import { useMutation, useQuery } from "@apollo/client";
+import SwipeCard from "./swipeCard/swipeCard";
+import { useQuery } from '@apollo/client';
+import Loader from '../Loader/Loader';
+import "./swipe.css";
 
-const db = [
-  {
-    name: "Richard Hendricks",
-    url: "https://media.istockphoto.com/vectors/person-gray-photo-placeholder-man-vector-id1133765772?k=20&m=1133765772&s=612x612&w=0&h=2X073i6UQf9Z6NRxena3em12vhr7I7nromkZk4mfEmk=",
-  },
-  {
-    name: "Erlich Bachman",
-    url: "https://media.istockphoto.com/vectors/person-gray-photo-placeholder-man-vector-id1133765772?k=20&m=1133765772&s=612x612&w=0&h=2X073i6UQf9Z6NRxena3em12vhr7I7nromkZk4mfEmk=",
-  },
-  {
-    name: "Monica Hall",
-    url: "",
-  },
-  {
-    name: "Jared Dunn",
-    url: "",
-  },
-  {
-    name: "Dinesh Chugtai",
-    url: "https://media.istockphoto.com/vectors/person-gray-photo-placeholder-man-vector-id1133765772?k=20&m=1133765772&s=612x612&w=0&h=2X073i6UQf9Z6NRxena3em12vhr7I7nromkZk4mfEmk=",
-  },
-];
 
 function Swiper() {
-  const [currentIndex, setCurrentIndex] = useState(db.length - 1);
-  const [lastDirection, setLastDirection] = useState();
+  const { loading, data } = useQuery(QUERY_ALL_USER)
 
-  const currentIndexRef = useRef(currentIndex);
-  const { loading, userData } = useQuery(QUERY_ALL_USER);
-  const users = userData || [];
-  console.log(users);
-  const childRefs = useMemo(
-    () =>
-      Array(db.length)
-        .fill(0)
-        .map((i) => React.createRef()),
-    []
-  );
-
-  const updateCurrentIndex = (val) => {
-    setCurrentIndex(val);
-    currentIndexRef.current = val;
-  };
-
-  const canGoBack = currentIndex < db.length - 1;
-
-  const canSwipe = currentIndex >= 0;
-
-  const swiped = (direction, nameToDelete, index) => {
-    setLastDirection(direction);
-    updateCurrentIndex(index - 1);
-  };
-
-  const outOfFrame = (name, idx) => {
-    currentIndexRef.current >= idx && childRefs[idx].current.restoreCard();
-  };
-
-  const swipe = async (dir) => {
-    if (canSwipe && currentIndex < db.length) {
-      await childRefs[currentIndex].current.swipe(dir); // Swipe the card!
-    }
-  };
-
-  // increase current index and show card
-  const goBack = async () => {
-    if (!canGoBack) return;
-    const newIndex = currentIndex + 1;
-    updateCurrentIndex(newIndex);
-    await childRefs[newIndex].current.restoreCard();
-  };
-
+  if (loading) {
+    return <Loader />;
+  }
   return (
-    <Row className="grid_swipe">
+    <Row className='grid_swipe'>
       <h1>React Tinder Card</h1>
-      <div className="cards_section">
-        <div className="cardContainer">
-          {db.map((user, index) => (
-            <TinderCard
-              ref={childRefs[index]}
-              className="swipe"
-              key={user.name}
-              onSwipe={(dir) => swiped(dir, user.name, index)}
-              onCardLeftScreen={() => outOfFrame(user.name, index)}
-            >
-              <div className="card">
-                <img src={user.url} alt={user.name} className="userImage"></img>
-                <h3>
-                  {user.name}, {user.age}
-                </h3>
-                <hr />
-                <div className="interest_section">
-                  {" "}
-                  {data.slice(0, 4).map((interest) => {
-                    return (
-                      <InterestButton
-                        disabled="true"
-                        icon={interest.icon}
-                        interest={interest.interest}
-                      />
-                    );
-                  })}
-                </div>
-                <hr />
-              </div>
-            </TinderCard>
-          ))}
-        </div>
-        <div className="buttons">
-          <button onClick={() => swipe("left")}>
-            <MdCancel size={60} />
-          </button>
-          <button onClick={() => goBack()}>
-            <FaUndo size={40} />
-          </button>
-          <button onClick={() => swipe("right")}>
-            <AiFillHeart size={50} />
-          </button>
-        </div>
-        {lastDirection ? (
-          <h2 key={lastDirection} className="infoText">
-            You swiped {lastDirection}
-          </h2>
-        ) : (
-          <h2 className="infoText">
-            Swipe a card or press a button to get Restore Card button visible!
-          </h2>
-        )}
+      <div className='cards_section'>
+
+          <SwipeCard profiles={data.users} />
+
       </div>
     </Row>
-  );
+  )
 }
 
-export default Swiper;
+export default Swiper

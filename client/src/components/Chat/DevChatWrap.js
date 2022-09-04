0cae4fb899d5dfc362acad2ebd0c02b473c6d6a5
Phillip <phillip.besse@gmail.com>
2022-09-03 21:14:10 -0400

    more cleanup; update seeds

--- client/src/components/Chat/DevChatWrap.js
+++ client/src/components/Chat/DevChatWrap.js
@@ -1,18 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useQuery } from "@apollo/client";
 import DevChat from './Chat';
 import PubNub from "pubnub";
 import { PubNubProvider } from "pubnub-react";
-import { useParams, Navigate } from "react-router-dom";
-import { QUERY_USER, QUERY_ME } from "../../utils/queries";
+import { QUERY_ME } from "../../utils/queries";
 import { RiContrastDropLine } from 'react-icons/ri';
 import Loader from '../Loader/Loader';
-import Auth from '../../utils/auth';
 import { getFormControlUnstyledUtilityClass } from '@mui/base';
 
 const DevChatWrap = () => {
-
-    // let pnUUID;
     let pnName;
 
     const { loading: loadme, data: profile } = useQuery(QUERY_ME);
@@ -25,24 +21,17 @@ const DevChatWrap = () => {
     if (!user) {
         return (
             <h4>
-                You need to be logged in to see this. Use the navigation links above to
-                sign up or log in!
+                You need to be logged in to slide into DMs.
             </h4>
         );
     }
-    // pnUUID = user._id;
-    // console.log(pnUUID);
     pnName = user.profile.firstName;
-    // console.log(user.profile.firstName);
 
     const pubnub = new PubNub(
         {
             publishKey: process.env.REACT_APP_MY_PUBLISH_KEY,
             subscribeKey: process.env.REACT_APP_MY_SUBSCRIBE_KEY,
-            // uuid: pnUUID,
             uuid: pnName
-            // uuid: useMyQuery(),
-            // userId: useMyQuery(),
         })
 
     return (
